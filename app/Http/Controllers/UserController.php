1f7e2973f327c7dbec02619cb9cfa836be80d676
0r2 <5547009@qq.com>
2017-05-11 13:05:42 +0800

    user setting

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,6 +5,7 @@
  * Date: 2017/1/4
  * Time: 下午2:41
  */
+
 namespace App\Http\Controllers;
 
 use Mail;
@@ -19,167 +20,186 @@ use Illuminate\Support\Facades\Session;
 class UserController extends Controller
 {
 
-    public function index()
-    {
+	public function index()
+	{
 
-    }
+	}
 
-    /**
-     * Auth 登陆 API
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
-    {
-        $rules = array(
-            'email' => 'required|email|max:255',
-            'passwd' => 'required|min:6',
-        );
-        $validator = validator(Input::get(), $rules);
-        if ($validator->fails()) {
-            $messages = $validator->messages()->first();
-            return Response::json(400, [], $messages);
-        }
-        $email = Input::get('email');
-        $passwd = Input::get('passwd');
-        $user = User::getUserByEmail($email);
-        if ($user) {   //登入验证
-            if ($user->status === 1) {
-                if (Hash::check($passwd, $user->passwd)) {
-                    return Response::json(1200, ['id' => $user->id], '登入成功');
-                }
-                return Response::json(1400, [], '登陆失败');
-            }
-            return Response::json(1202, ['id' => $user->id], '账户未激活或已被冻结');
-        }
-        //注册
-        $res = User::createUser($email, $passwd);
-        if (!$res) {
-            return Response::json(1401, [], '注册失败');
-        }
-        $str = "欢迎使用Tiki,请打开以下链接激活账号\n激活地址：http://tiki.im/activation?id=" .
-            $res->id . "&token=" . $res->token;
-        $res = Mail::raw($str, function ($message) use ($email) {
-            $message->subject('激活');
-            $message->to($email);
-        });
-        return Response::json(1201, [], '注册成功');
-    }
+	/**
+	 * Auth 登陆 API
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function login()
+	{
+		$rules = array(
+			'email'  => 'required|email|max:255',
+			'passwd' => 'required|min:6',
+		);
+		$validator = validator(Input::get(), $rules);
+		if($validator->fails()){
+			$messages = $validator->messages()->first();
+			return Response::json(400, [], $messages);
+		}
+		$email = Input::get('email');
+		$passwd = Input::get('passwd');
+		$user = User::getUserByEmail($email);
+		if($user){   //登入验证
+			if($user->status === 1){
+				if(Hash::check($passwd, $user->passwd)){
+					return Response::json(1200, ['id' => $user->id], '登入成功');
+				}
+				return Response::json(1400, [], '登陆失败');
+			}
+			return Response::json(1202, ['id' => $user->id], '账户未激活或已被冻结');
+		}
+		//注册
+		$res = User::createUser($email, $passwd);
+		if(!$res){
+			return Response::json(1401, [], '注册失败');
+		}
+		$str = "欢迎使用Tiki,请打开以下链接激活账号\n激活地址：http://tiki.im/activation?id=" .
+			$res->id . "&token=" . $res->token;
+		$res = Mail::raw($str, function ($message) use ($email) {
+			$message->subject('激活');
+			$message->to($email);
+		});
+		return Response::json(1201, [], '注册成功');
+	}
 
-    /**
-     * Auth 获取用户信息 API
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function userInfo()
-    {
-        $uid = Input::get('uid');
-        $user = User::getUserById($uid);
-        if (empty($user)) {
-            return Response::json(1403, [], '用户信息获取失败');
-        }
-        return Response::json(1203, [
-            'uid' => $user['id'],
-            'avatar' => '',
-            'email' => $user['email'],
-            'name' => $user['name'],
-            'status' => $user['status'],
-            'created_at' => $user['created_at'],
-        ], '用户信息');
-    }
+	/**
+	 * Auth 获取用户信息 API
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function userInfo()
+	{
+		$uid = Input::get('uid');
+		$user = User::getUserById($uid);
+		if(empty($user)){
+			return Response::json(1403, [], '用户信息获取失败');
+		}
+		return Response::json(1203, [
+			'uid'        => $user['id'],
+			'avatar'     => '',
+			'email'      => $user['email'],
+			'name'       => $user['name'],
+			'status'     => $user['status'],
+			'created_at' => $user['created_at'],
+		], '用户信息');
+	}
 
-    /**
-     * 登入回调函数  用户验证
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function authCallback()
-    {
-        $code = Input::get('code');
-        $uid = Input::get('uid');
-        if (empty($code) || empty($uid)) {
-            //return Response::json(1404, [], '请求信息不全');
-            return view('login')->with('msg', '请求信息不全');
-        }
-        //auth -> token获取
-        $token_url = 'https://oauth.tiki.im/token?code=' . $code .
-            '&client_id=test&grant_type=authorization_code&client_secret=12jh3gas623g&redirect_uri=https://tiki.im/login/callback';
-        $token_info = json_decode(Curl::get($token_url));
-        if (empty($token_info->access_token)) {
-            //return Response::json(1405, [], 'Auth token获取失败');
-            return view('login')->with('msg', 'Auth token获取失败');
-        }
-        //token -> 用户信息获取
-        $user_url = 'https://oauth.tiki.im/info?code=' . $token_info->access_token . '&uid=' . $uid;
-        $user_info = json_decode(Curl::get($user_url));
-        if (empty($user_info->uid)) {
-            //return Response::json(1406, [], 'Auth 用户信息获取失败');
-            return view('login')->with('msg', 'Auth 用户信息获取失败');
-        }
-        Session::put('user', $user_info);
-        if(Input::get('state') == 2){
-            return redirect('https://pan.tiki.im');
-        }
-        return redirect('/center');
-    }
+	/**
+	 * 登入回调函数  用户验证
+	 * @return \Illuminate\Http\JsonResponse
+	 */
+	public function authCallback()
+	{
+		$code = Input::get('code');
+		$uid = Input::get('uid');
+		if(empty($code) || empty($uid)){
+			//return Response::json(1404, [], '请求信息不全');
+			return view('login')->with('msg', '请求信息不全');
+		}
+		//auth -> token获取
+		$token_url = 'https://oauth.tiki.im/token?code=' . $code .
+			'&client_id=test&grant_type=authorization_code&client_secret=12jh3gas623g&redirect_uri=https://tiki.im/login/callback';
+		$token_info = json_decode(Curl::get($token_url));
+		if(empty($token_info->access_token)){
+			//return Response::json(1405, [], 'Auth token获取失败');
+			return view('login')->with('msg', 'Auth token获取失败');
+		}
+		//token -> 用户信息获取
+		$user_url = 'https://oauth.tiki.im/info?code=' . $token_info->access_token . '&uid=' . $uid;
+		$user_info = json_decode(Curl::get($user_url));
+		if(empty($user_info->uid)){
+			//return Response::json(1406, [], 'Auth 用户信息获取失败');
+			return view('login')->with('msg', 'Auth 用户信息获取失败');
+		}
+		Session::put('user', $user_info);
+		if(Input::get('state') == 2){
+			return redirect('https://pan.tiki.im');
+		}
+		return redirect('/center');
+	}
 
-    /**
-     * 账号激活
-     * @return $this
-     */
-    public function activation()
-    {
-        $id = Input::get('id');
-        $token = Input::get('token');
-        $user = User::getUserById($id);
-        if ($user && $user->token == $token) {
-            return view('user.activation')->with([
-                'title' => '激活账号',
-                'msg' => '请设置密码',
-                'id' => $id,
-                'token' => $token
-            ]);
-        }
-        return view('errors.show')->with([
-            'title' => '激活账号',
-            'msg' => '无效链接',
-        ]);
-    }
+	/**
+	 * 账号激活
+	 * @return $this
+	 */
+	public function activation()
+	{
+		$id = Input::get('id');
+		$token = Input::get('token');
+		$user = User::getUserById($id);
+		if($user && $user->token == $token){
+			return view('user.activation')->with([
+				'title' => '激活账号',
+				'msg'   => '请设置密码',
+				'id'    => $id,
+				'token' => $token
+			]);
+		}
+		return view('errors.show')->with([
+			'title' => '激活账号',
+			'msg'   => '无效链接',
+		]);
+	}
 
-    /**
-     * 设置密码
-     * @return $this|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
-     */
-    public function setPasswdByToken()
-    {
-        $rules = array(
-            'passwd' => 'required|min:6|confirmed',
-        );
-        $validator = validator(Input::get(), $rules);
-        if ($validator->fails()) {
-            $msg = $validator->messages()->first();
-            return back()->with(['error_msg' => $msg]);
-        }
-        $id = Input::get('id');
-        $token = Input::get('token');
-        $passwd = Input::get('passwd');
-        $user = User::getUserById($id);
-        if ($user && $token == $user->token) {
-            $data = [
-                'status' => 1,
-                'passwd' => Hash::make($passwd),
-                'token' => '',
-            ];
-            User::updateInfo($data, ['id' => $id]);
-            return redirect('login');
-        }
-        return view('errors.show')->with([
-            'title' => '激活账号',
-            'msg' => '无效链接',
-        ]);
-    }
+	/**
+	 * 设置密码
+	 * @return $this|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+	 */
+	public function setPasswdByToken()
+	{
+		$rules = array(
+			'passwd' => 'required|min:6|confirmed',
+		);
+		$validator = validator(Input::get(), $rules);
+		if($validator->fails()){
+			$msg = $validator->messages()->first();
+			return back()->with(['error_msg' => $msg]);
+		}
+		$id = Input::get('id');
+		$token = Input::get('token');
+		$passwd = Input::get('passwd');
+		$user = User::getUserById($id);
+		if($user && $token == $user->token){
+			$data = [
+				'status' => 1,
+				'passwd' => Hash::make($passwd),
+				'token'  => '',
+			];
+			User::updateInfo($data, ['id' => $id]);
+			return redirect('login');
+		}
+		return view('errors.show')->with([
+			'title' => '激活账号',
+			'msg'   => '无效链接',
+		]);
+	}
 
-    public function logout()
-    {
-        Session::flush();
-        return redirect('https://tiki.im');
-    }
+	public function setting()
+	{
+		$user = Session::get('user');
+		dd($user);
+		$data = [
+			'name' => $user->name,
+			'email'
+		];
+		return view('tiki.index')->with($data);
+	}
+
+	public function NameSetting()
+	{
+		$data = [
+			Input::get(''),
+		];
+		dd($data);
+	}
+
+	public function logout()
+	{
+		Session::flush();
+		return redirect('https://tiki.im');
+	}
 
 }
--- resources/views/user/setting.blade.php
+++ resources/views/user/setting.blade.php
@@ -6,50 +6,60 @@
 <div class="container" style="margin-top: 70px">
     <div class="row m-t-40">
         <div class="col-xs-8 col-xs-offset-2">
-            <h4>账号设置</h4>
+            <h4>ID 设置</h4>
             <div class="col-xs-9">
-                <form>
+                <form action="/idSetting" method="post">
+                    <div class="form-group">
+                        <label for="name">Email</label>
+                        <input type="text" class="form-control" id="email" name="email" disabled>
+                    </div>
                     <div class="form-group">
                         <label for="name">Name</label>
-                        <input type="text" class="form-control" id="name">
-                    </div>
-                    <button type="submit" class="btn btn-sm w-md btn-success pull-right">更新</button>
-                </form>
-            </div>
-            <div class="col-xs-9">
-                <form>
-                    <div class="form-group">
-                        <label for="nickname">Nick</label>
-                        <input type="text" class="form-control" id="nickname">
-                    </div>
-                    <div class="form-group">
-                        <label for="url">URL</label>
-                        <input type="text" class="form-control" id="url">
-                    </div>
-                    <div class="form-group">
-                        <label for="bio">简介</label>
-                        <textarea class="form-control" rows="3" name="bio" id="bio"></textarea>
-                    </div>
-                    <div class="form-group">
-                        <label for="url">所在公司</label>
-                        <input type="text" class="form-control" id="company">
-                    </div>
-                    <div class="form-group">
-                        <label for="location">所在地</label>
-                        <input type="text" class="form-control" id="location">
+                        <input type="text" class="form-control" id="name" name="name">
                     </div>
                     <button type="submit" class="btn btn-sm w-md btn-success pull-right">更新</button>
                 </form>
             </div>
             <div class="col-xs-3">
-                <label>头像</label>
+                <label> 头像 </label>
                 <div style="height: 200px; width: 200px; max-width: 100%">
-                    <img src="https://pbs.twimg.com/profile_images/831518614561837058/ytvJfYOx_400x400.jpg" class="img-rounded img-responsive">
+                    <img src="https://pbs.twimg.com/profile_images/831518614561837058/ytvJfYOx_400x400.jpg"
+                         class="img-rounded img-responsive">
                     <form enctype="multipart/form-data">
                         <input type="file" name="avatar" id="real-file-upload" style="display:none">
-                        <button type="button" onclick="$('#real-file-upload').click()" class="center-block m-t-10 btn btn-sm w-md btn-default">上传新头像</button>
+                        <button type="button" onclick="$('#real-file-upload').click()"
+                                class="center-block m-t-10 btn btn-sm w-md btn-default">上传新头像
+                        </button>
                     </form>
                 </div>
+            </div>
+        </div>
+        <div class="col-xs-8 col-xs-offset-2">
+            <h4>用户资料</h4>
+            <div class="col-xs-9">
+                <form action="/userSetting" method="post">
+                    <div class="form-group">
+                        <label for="nickname">Nick</label>
+                        <input type="text" class="form-control" id="nick" name="nick">
+                    </div>
+                    <div class="form-group">
+                        <label for="url">Url</label>
+                        <input type="text" class="form-control" id="url" name="url">
+                    </div>
+                    <div class="form-group">
+                        <label for="bio">简介</label>
+                        <textarea class="form-control" rows="3" id="bio" name="bio"></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="url">所在公司</label>
+                        <input type="text" class="form-control" id="company" name="company">
+                    </div>
+                    <div class="form-group">
+                        <label for="location">所在地</label>
+                        <input type="text" class="form-control" id="location" name="location">
+                    </div>
+                    <button type="submit" class="btn btn-sm w-md btn-success pull-right">更新</button>
+                </form>
             </div>
         </div>
     </div>
