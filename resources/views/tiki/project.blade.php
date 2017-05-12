91412103ac54f0b405e119e5b611f9431be75ac7
oif <neowiz@icloud.com>
2017-05-12 18:46:37 +0800

    s

--- resources/views/tiki/project.blade.php
+++ resources/views/tiki/project.blade.php
@@ -6,7 +6,7 @@
 <div class="container">
     <div class="row m-t-40">
         <div class="col-xs-12">
-            <h3>{{ $org }} / {{ $name }}
+            <h3>{{ $data['org'] }} / {{ $data['name'] }}
                 <div class="pull-right">
                     <a class="btn btn-sm btn-default" href="#" role="button"><i class="fa fa-star" aria-hidden="true"></i> Start 23,123</a>
                 </div>
@@ -14,8 +14,8 @@
             <p class="text-muted">Updated 5 months ago</p>
             <div class="jumbotron">
                 <p><a class="btn btn-success btn-md w-lg pull-right">立即阅读</a></p>
-                <h1>{{ $name }}</h1>
-                <p>{{ $description }}</p>
+                <h1>{{ $data['name'] }}</h1>
+                <p>{{ $data['description'] }}</p>
             </div>
         </div>
     </div>
