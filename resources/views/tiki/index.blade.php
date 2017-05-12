16efe8a5dad0d46679980113346cc7f535f16e67
oif <neowiz@icloud.com>
2017-05-12 19:37:04 +0800

    s

--- resources/views/tiki/index.blade.php
+++ resources/views/tiki/index.blade.php
@@ -30,11 +30,11 @@
                 @if(count($path) > 1)
                 @if(count($path) == 2)
                 <!-- 组织 -->
-                <a href="/orgSetting/{{ $obId }}" class="btn btn-sm btn-default">设置</a>
+                <a href="/orgSetting/{{ $obId }}" class="btn btn-sm btn-default">组织设置</a>
                 <!-- 项目 -->
                 @elseif(count($path) > 2)
                 <a href="/project/{{ $obId }}" class="btn btn-sm btn-default">主页</a>
-                <a href="/projectSetting/{{ $obId }}" class="btn btn-sm btn-default">设置</a>
+                <a href="/projectSetting/{{ $obId }}" class="btn btn-sm btn-default">项目设置</a>
                 @endif
                 @endif
             </div>
